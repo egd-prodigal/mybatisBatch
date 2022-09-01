53e63be7bfcd7c34116fcfd1c56c50206072505a
yeemin <yeeminshon@outlook.com>
2022-09-01 10:57:31 +0800

    自动装配

--- sample/src/main/java/io/github/egd/prodigal/sample/MybatisBatchApplication.java
+++ sample/src/main/java/io/github/egd/prodigal/sample/MybatisBatchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableMybatisBatch
+//@EnableMybatisBatch
 public class MybatisBatchApplication {
 
 	public static void main(String[] args) {

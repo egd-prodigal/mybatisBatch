33b611cf2d273f4da0a89db64c4426c05254fc1a
yeemin <yeeminshon@outlook.com>
2022-09-01 14:12:56 +0800

    兼容一下老版本的方法

--- mybatis-batch-starter/src/main/java/io/github/egd/prodigal/mybatis/batch/context/SpringBatchSqlSessionBuilder.java
+++ mybatis-batch-starter/src/main/java/io/github/egd/prodigal/mybatis/batch/context/SpringBatchSqlSessionBuilder.java
@@ -1,18 +1,49 @@
 package io.github.egd.prodigal.mybatis.batch.context;
 
 import io.github.egd.prodigal.mybatis.batch.core.BatchSqlSessionBuilder;
+import org.apache.ibatis.exceptions.PersistenceException;
 import org.apache.ibatis.session.ExecutorType;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.MyBatisExceptionTranslator;
+import org.mybatis.spring.MyBatisSystemException;
 import org.mybatis.spring.SqlSessionUtils;
+import org.springframework.dao.DataAccessException;
+import org.springframework.dao.support.PersistenceExceptionTranslator;
+import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
+import org.springframework.transaction.TransactionException;
+
+import java.sql.SQLException;
 
 public class SpringBatchSqlSessionBuilder implements BatchSqlSessionBuilder {
 
     private final SqlSessionFactory sqlSessionFactory;
 
-    private final MyBatisExceptionTranslator exceptionTranslator = new MyBatisExceptionTranslator(SQLErrorCodeSQLExceptionTranslator::new, false);
+//    private final MyBatisExceptionTranslator exceptionTranslator = new MyBatisExceptionTranslator(SQLErrorCodeSQLExceptionTranslator::new, false);
+
+    private final PersistenceExceptionTranslator exceptionTranslator = new PersistenceExceptionTranslator() {
+
+        final SQLErrorCodeSQLExceptionTranslator translator = new SQLErrorCodeSQLExceptionTranslator();
+
+        @Override
+        public DataAccessException translateExceptionIfPossible(RuntimeException ex) {
+            if (ex instanceof PersistenceException) {
+                if (ex.getCause() instanceof PersistenceException) {
+                    ex = (PersistenceException) ex.getCause();
+                }
+                if (ex.getCause() instanceof SQLException) {
+                    String task = ex.getMessage() + "\n";
+                    SQLException se = (SQLException) ex.getCause();
+                    DataAccessException dae = this.translator.translate(task, null, se);
+                    return dae != null ? dae : new UncategorizedSQLException(task, null, se);
+                } else if (ex.getCause() instanceof TransactionException) {
+                    throw (TransactionException) ex.getCause();
+                }
+                return new MyBatisSystemException(ex);
+            }
+            return null;
+        }
+    };
 
     public SpringBatchSqlSessionBuilder(SqlSessionFactory sqlSessionFactory) {
         this.sqlSessionFactory = sqlSessionFactory;

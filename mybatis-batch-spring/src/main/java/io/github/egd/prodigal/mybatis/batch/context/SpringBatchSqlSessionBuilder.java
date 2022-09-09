aca03a93146c4a7e0d086666ff28af72bfbc6ae6
yeemin <yeeminshon@outlook.com>
2022-09-09 11:12:09 +0800

    update readme flushstatement参数功能调整

--- mybatis-batch-spring/src/main/java/io/github/egd/prodigal/mybatis/batch/context/SpringBatchSqlSessionBuilder.java
+++ mybatis-batch-spring/src/main/java/io/github/egd/prodigal/mybatis/batch/context/SpringBatchSqlSessionBuilder.java
@@ -34,18 +34,30 @@ public class SpringBatchSqlSessionBuilder implements BatchSqlSessionBuilder {
     @Override
     public SqlSession build(boolean flushStatements) {
         SqlSessionFactory sqlSessionFactory = sqlSessionTemplate.getSqlSessionFactory();
-        if (flushStatements) {
-            SqlSessionUtils.getSqlSession(sqlSessionFactory).flushStatements();
-        }
-        // 重新开启一个SqlSession，基于sqlSessionTemplate创建
-        SqlSession sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH, false);
+        SqlSession sqlSession;
         // 如果存在spring管理的事务，则交给spring管理
         boolean synchronizationActive = TransactionSynchronizationManager.isSynchronizationActive();
         if (synchronizationActive) {
-            Object resource = TransactionSynchronizationManager.getResource(sqlSessionTemplate);
-            if (resource == null) {
+            // 先从事务里获取sqlSession
+            sqlSession = (SqlSession) TransactionSynchronizationManager.getResource(sqlSessionTemplate);
+            boolean batchSqlSessionOpened = sqlSession != null;
+            if (batchSqlSessionOpened && flushStatements) {
+                sqlSession.flushStatements();
+                // 事实上，下面的执行基本无效
+                SqlSessionUtils.getSqlSession(sqlSessionFactory).flushStatements();
+            }
+            if (!batchSqlSessionOpened) {
+                // 重新开启一个SqlSession，基于sqlSessionTemplate创建
+                sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH, false);
                 new BatchSqlSessionSynchronization(this.sqlSessionTemplate, sqlSession).register();
             }
+        } else {
+            if (flushStatements) {
+                // 事实上，下面的执行基本无效
+                SqlSessionUtils.getSqlSession(sqlSessionFactory).flushStatements();
+            }
+            // 重新开启一个SqlSession，基于sqlSessionTemplate创建
+            sqlSession = sqlSessionFactory.openSession(ExecutorType.BATCH, false);
         }
         return sqlSession;
     }
@@ -53,7 +65,7 @@ public class SpringBatchSqlSessionBuilder implements BatchSqlSessionBuilder {
     /**
      * 提交事务
      *
-     * @param sqlSession sqlSession
+     * @param sqlSession      sqlSession
      * @param flushStatements 是否刷盘
      */
     @Override
@@ -62,15 +74,9 @@ public class SpringBatchSqlSessionBuilder implements BatchSqlSessionBuilder {
             BatchSqlSessionBuilder.super.commit(sqlSession, flushStatements);
         } else {
             if (flushStatements) {
-                // 批量处理的SqlSession和常规的SqlSession都刷入数据库，使会话能够共享执行结果
+                // 批量处理的SqlSession刷入数据库，使会话能够共享执行结果
                 // 在实际使用中，建议批量的操作在业务的最后执行，最好是异步实现
                 sqlSession.flushStatements();
-                if (TransactionSynchronizationManager.getResource(sqlSessionTemplate.getSqlSessionFactory()) != null) {
-                    SqlSession session = SqlSessionUtils.getSqlSession(sqlSessionTemplate.getSqlSessionFactory());
-                    if (session != null) {
-                        session.flushStatements();
-                    }
-                }
             }
         }
     }
